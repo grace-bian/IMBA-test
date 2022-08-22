23b8872439823164669e3fe85e0ab90234a275ee
Grace Bian <grace.bian@outlook.com>
2022-08-22 23:07:48 +1000

    Modified gihub workflow yaml

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,13 +1,13 @@
-variable "aws_access_key" {
-  default = "AKIA5MZOR24T6TBOUCMC"
-}
-variable "aws_secret_key" {
-  default = "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"
-}
+# variable "aws_access_key" {
+#   default = "AKIA5MZOR24T6TBOUCMC"
+# }
+# variable "aws_secret_key" {
+#   default = "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"
+# }
 
-variable "region" {
-  default = "ap-southeast-2"
-}
+# variable "region" {
+#   default = "ap-southeast-2"
+# }
 
 variable "bucket_name" {
   default = "imba-aws-source-code"
